29a39f42c63f8567cd1cee01bf34d1d4f708a776
anav <anav2096@gmail.com>
2022-05-08 19:24:43 -0400

    add manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,19 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./", 
-  "./public/js/index.html",
-  "./public/js/idb.js",
-  "./public/index.html",
-  "./public/css/style.css",
-  "./public/icons/icon-72x72.png",
-  "./public/icons/icon-96x96.png",
-  "./public/icons/icon-128x128.png",
-  "./public/icons/icon-144x144.png",
-  "./public/icons/icon-152x152.png",
-  "./public/icons/icon-192x192.png",
-  "./public/icons/icon-384x384.png",
-  "./public/icons/icon-512x512.png",
-  "./routes/api.js",
+  "./js/index.js",
+  "./js/idb.js",
+  "./index.html",
+  "./css/style.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+  
 
 ];
 
